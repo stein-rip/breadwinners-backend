0be11e5765cf00539db190f0402be0b1eb64f236
Dana Tanner <nielsen.danatanner@gmail.com>
2023-03-17 16:57:00 -0400

    routes

--- functions/src/models/Favorite.ts
+++ functions/src/models/Favorite.ts
@@ -1,17 +1,27 @@
 import { ObjectId } from "mongodb";
-
-interface Original {
-  url: string;
-}
-
-interface Images {
-  original: Original;
+interface JobHighlights {
+  Qualifications: string[];
+  Responsibilities: string[];
+  Benefits: null | string[];
 }
 
 interface Job {
-  id: string;
-  title: string;
-  images: Images;
+  employer_name: string;
+  employer_logo: string;
+  employer_company_type: string;
+  job_id: string;
+  job_title: string;
+  job_description: string;
+  job_is_remote: boolean;
+  job_posted_at_timestamp: number;
+  job_posted_at_datetime_utc: string;
+  job_city: null | string;
+  job_state: null | string;
+  job_country: null | string;
+  // job_benefits?: JobBenefits | null;
+  job_offer_expiration_datetime_utc: string;
+  job_offer_expiration_timestamp: number;
+  job_highlights: JobHighlights;
 }
 
 export default interface Favorite {
